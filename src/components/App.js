1fee6eaee80fbf73d2bd81b42de12b11767ad74f
jkan@ucdavis.edu <jkan@ucdavis.edu>
2022-03-14 17:53:25 -0700

    Imports image manipulation form into app

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,12 @@
 import '../styles/App.css';
+import ImageManipulationForm from './forms/ImageManipulationForm';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello World!</h1>
         <img src="https://assets.imgix.net/unsplash/bridge.jpg" id="bridge" alt="golden-gate-bridge"></img>
-        
+        <ImageManipulationForm />
       </header>
     </div>
   );
