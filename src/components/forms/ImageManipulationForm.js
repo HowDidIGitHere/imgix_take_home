46500545da721a2904a4cc46a0a5afad9a83d02e
jkan@ucdavis.edu <jkan@ucdavis.edu>
2022-03-15 16:05:41 -0700

    Adds temporary state in image manipulation form

--- src/components/forms/ImageManipulationForm.js
+++ src/components/forms/ImageManipulationForm.js
@@ -1,26 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function ImageManipulationForm(props) {
+
+  const [tempVals, setTempVals] = useState({
+    txt: props.formFields.txt,
+    blend: props.formFields.blend
+  });
 
   const handleUserInput = type => {
     return (e) => {
       e.preventDefault();
       e.stopPropagation();
-      props.setFormFields({ ...props.formFields, [type]: e.currentTarget.value });
+      setTempVals({ ...tempVals, [type]: e.currentTarget.value });
     };
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    e.stopPropagation();
+    props.setFormFields({ ...props.formFields, ...tempVals });
   };
 
   return (
     <form className="image-overlay-form">
       <label className="text-overlay">
         <p>Text:</p>
-        <input type="text" value={props.formFields.txt} placeholder="Add Text!" onChange={handleUserInput('txt')} />
+        <input type="text" value={tempVals.txt} placeholder="Add Text!" onChange={handleUserInput('txt')} />
       </label>
 
       <label className="blend-color-overlay">
         <p>Blend:</p>
-        <input type="color" value={props.formFields.blend} onChange={handleUserInput('blend')} />
+        <input type="color" value={tempVals.blend} onChange={handleUserInput('blend')} />
       </label>
+
+      <button onClick={handleSubmit}>Make Changes!</button>
     </form>
   );
 }
