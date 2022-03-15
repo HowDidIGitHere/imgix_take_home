01698ec73067d97b856f5b2fecc610f845139690
jkan@ucdavis.edu <jkan@ucdavis.edu>
2022-03-14 18:19:36 -0700

    Refactors react app state component composition to pass down image form fields from App to form component

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,37 @@
+import React, { useState } from 'react';
 import '../styles/App.css';
+import Imgix from "react-imgix";
 import ImageManipulationForm from './forms/ImageManipulationForm';
 
 function App() {
+
+  const [formFields, setFormFields] = useState({
+    txt: "",
+    blend: "#4400bb"
+  });
+
+  const handleUserInput = type => {
+    return (e) => {
+      e.preventDefault();
+      e.stopPropagation();
+      setFormFields({ ...formFields, [type]: e.currentTarget.value });
+    };
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src="https://assets.imgix.net/unsplash/bridge.jpg" id="bridge" alt="golden-gate-bridge"></img>
-        <ImageManipulationForm />
+        <Imgix 
+          src="https://assets.imgix.net/unsplash/bridge.jpg" 
+          sizes="60rem"
+          imgixParams={{ 
+            txt: formFields.txt,
+            txtcolor: "white",
+            blend: formFields.blend
+          }}
+          alt="golden-gate-bridge"
+        />
+        <ImageManipulationForm formFields={formFields} setFormFields={setFormFields} handleUserInput={handleUserInput} />
       </header>
     </div>
   );
--- src/components/forms/ImageManipulationForm.js
+++ src/components/forms/ImageManipulationForm.js
@@ -1,32 +1,17 @@
-import React, { useState } from "react";
+import React from "react";
 
-export default function ImageManipulationForm() {
-
-  const [txt, setTxt] = useState("");
-  const [blend, setBlend] = useState("#4400bb");
-
-  const handleUserInput = type => {
-    return (e) => {
-      e.preventDefault();
-      e.stopPropagation();
-      if (type === "txt") {
-        setTxt(e.currentTarget.value);
-      } else {
-        setBlend(e.currentTarget.value);
-      }
-    };
-  };
+export default function ImageManipulationForm(props) {
 
   return (
     <form className="image-overlay-form">
       <label className="text-overlay">
         <p>Text</p>
-        <input type="text" value={txt} onChange={handleUserInput('txt')} />
+        <input type="text" value={props.formFields.txt} placeholder="Add Text!" onChange={props.handleUserInput('txt')} />
       </label>
 
       <label className="blend-color-overlay">
         <p>Blend</p>
-        <input type="color" value={blend} onChange={handleUserInput('blend')} />
+        <input type="color" value={props.formFields.blend} onChange={props.handleUserInput('blend')} />
       </label>
     </form>
   );
