b93aaf061aae8d2276d14fb297067d598d936e0b
jkan@ucdavis.edu <jkan@ucdavis.edu>
2022-03-15 17:48:39 -0700

    Refactors state and App DOM hierarchy

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,13 @@
 import React, { useState } from 'react';
 import '../styles/App.css';
+import '../styles/image.css'
 import Imgix from "react-imgix";
 import ImageManipulationForm from './forms/ImageManipulationForm';
 
 function App() {
 
-  const [formFields, setFormFields] = useState({
-    txt: "Check out this sweet bridge",
+  const [imgSettings, setImgSettings] = useState({
+    txt: "Check out this sweet bridge!",
     txtcolor: "#fff",
     txtsize: "64",
     txtalign: "center middle",
@@ -19,14 +20,15 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1 className="title">See What Imgix Can Do!</h1>
         <div className="image-wrapper">
           <Imgix 
             src="https://assets.imgix.net/unsplash/bridge.jpg" 
-            imgixParams={formFields}
+            imgixParams={imgSettings}
             alt="golden-gate-bridge"
           />
+          <ImageManipulationForm imgSettings={imgSettings} setImgSettings={setImgSettings} />
         </div>
-        <ImageManipulationForm formFields={formFields} setFormFields={setFormFields} />
       </header>
     </div>
   );
